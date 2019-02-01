1d94860326586423679bc4f0915c718991cca0b4
uday <udaybojja16@gmail.com>
2019-02-01 16:58:06 +0530

    Menu scene is coded

--- Assets/3DGamekitLite/Scripts/Managers/LevelManager.cs
+++ Assets/3DGamekitLite/Scripts/Managers/LevelManager.cs
@@ -9,7 +9,8 @@ public class LevelManager : MonoBehaviour
     public static LevelManager Singleton { get; private set; }
 
     public Button[] levelButtons;
-    // Start is called before the first frame update
+    public GameObject[] levelLocks;
+    private int levelUnlocked;
 
     private void Awake()
     {
@@ -27,7 +28,25 @@ public class LevelManager : MonoBehaviour
     }
     void Start()
     {
-        
+        // here we should get the integer value of levelUnlocked from JSON
+    }
+
+    //Unlock levels
+    void Unlocklevel()
+    {
+        for (int i = 0; i < levelLocks.Length; i++)
+        {
+            if (i <= levelUnlocked)
+            {
+                levelButtons[i].interactable = true;
+                levelLocks[i].gameObject.SetActive(false);
+            }
+            else
+            {
+                levelButtons[i].interactable = false;
+                levelLocks[i].gameObject.SetActive(true);
+            }
+        }
     }
 
     // Update is called once per frame
---
+++ Assets/3DGamekitLite/Scripts/Managers/Node.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public int nodeIndex;
+    public GameObject starsHolder; //Stars holder
+    private List<GameObject> starsImagesList = new List<GameObject>(); //List with stars
+
+    public GameObject firstStarImage; //first star image reference
+    public GameObject secondStarImage; //second star image reference
+    public GameObject thirdStarImage; //third star image reference
+
+    private int numberOfStars;
+    // Start is called before the first frame update
+    void Start()
+    {
+        starsImagesList.Clear();
+        starsImagesList.Add(firstStarImage);
+        starsImagesList.Add(secondStarImage);
+        starsImagesList.Add(thirdStarImage);
+        UpdateStars();
+    }
+
+
+    public void UpdateStars()
+    {
+        //Based on number of stars activate the images
+        starsHolder.gameObject.SetActive(true);
+   
+        // Here we should get the number of stars from JSON 
+        if (starsImagesList.Count > 0)
+        {
+            //Lets set all the stars off
+            for (int i = 0; i < starsImagesList.Count; i++)
+            {
+                starsImagesList[i].gameObject.SetActive(false);
+            }
+
+            //For the number of stars lets activate them
+            for (int i = 0; i < numberOfStars; i++)
+            {
+                starsImagesList[i].gameObject.SetActive(true);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
